2bb9a513a31fc83db430326061609344a66a9c81
panchumarthi03 <61387329+panchumarthi03@users.noreply.github.com>
2020-04-09 12:38:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ 
+if(isset($_POST['submit'])){
+	$name=$_POST['name'];
+	$emailFrom=$_POST['email'];
+	$message=$_POST['message'];
+	
+	
+    $emailTo="panchumarthilavanya94@gmail.com";
+	$headers="From:" .$emailFrom;
+	$txt="You have received a message from".$name.".\n\n".$message;
+
+	mail($emailTo, $txt, $headers);
+	header("Location:index.php?mailsend");
+	
+}
+?>
